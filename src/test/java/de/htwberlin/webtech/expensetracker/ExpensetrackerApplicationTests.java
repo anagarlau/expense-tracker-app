eaf72ae42ef3343156fa2b441f831b23f31ea249
Ana-Maria Garlau <garlauanamaria@gmail.com>
2022-04-18 20:44:47 +0200

    init

---
+++ src/test/java/de/htwberlin/webtech/expensetracker/ExpensetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
